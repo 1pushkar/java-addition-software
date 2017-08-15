c760fed495c3549d0c54e4c502b61196bb59447c
1pushkar <1pushkarkariyare@gmail.com>
2017-08-16 02:16:23 +0530

    i maked my first creation
    there is i am ceation a normal thing

---
+++ adding.java
@@ -0,0 +1,14 @@
+class a
+{
+
+
+public static void main(String args][]){
+
+int a,b,c;
+Scanner scan=new Scanner(System.in);
+a=scan.nextInt();
+b=scan.nextInt();
+c=a+b;
+System.out.println(c);
+}
+}
